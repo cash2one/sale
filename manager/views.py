016b1158366357d84bcd8f849d43f85ceb7414d4
zhanghaoyan <zhanghaoyan@elex-tech.com>
2017-05-20 11:44:12 +0800

    debug for deal

--- manager/views.py
+++ manager/views.py
@@ -1,125 +1,126 @@
 # -*- coding:utf-8 -*-
 from django.shortcuts import render,HttpResponse,redirect
 from django import forms
-from django.forms import widgets
+from django.forms import widgets as dwidgets
 from manager import models
 import json,datetime,time
+from django.forms import fields
 
 # Create your views here.
 
 class AddProduct(forms.Form):
     pname = forms.CharField(
         error_messages={"required":"名称不能为空"},
-        widget=widgets.TextInput(attrs={"class":"form-control","id":"name","placeholder":"商品名称"})
+        widget=dwidgets.TextInput(attrs={"class":"form-control","id":"name","placeholder":"商品名称"})
     )
     cost = forms.CharField(
         error_messages={"required":"成本不能为空"},
-        widget=widgets.TextInput(attrs={"class":"form-control","id":"cost","placeholder":"进价"})
+        widget=dwidgets.TextInput(attrs={"class":"form-control","id":"cost","placeholder":"进价"})
     )
     price = forms.CharField(
         error_messages={"required":"售价不能为空"},
-        widget=widgets.TextInput(attrs={"class":"form-control","id":"price","placeholder":"售价"})
+        widget=dwidgets.TextInput(attrs={"class":"form-control","id":"price","placeholder":"售价"})
     )
     number = forms.CharField(
         error_messages={"required":"进货量不能为空"},
-        widget=widgets.TextInput(attrs={"class":"form-control","id":"number","placeholder":"进货量"})
+        widget=dwidgets.TextInput(attrs={"class":"form-control","id":"number","placeholder":"进货量"})
     )
     stok = forms.CharField(
-        widget=widgets.TextInput(attrs={"id":"stok","style":"display: none"})
+        widget=dwidgets.TextInput(attrs={"id":"stok","style":"display: none"})
     )
 
 class AddStaff(forms.Form):
     sname = forms.CharField(
         error_messages={"required":"姓名不能为空"},
-        widget=widgets.TextInput(attrs={"class":"form-control","id":"sname","placeholder":"姓名"})
+        widget=dwidgets.TextInput(attrs={"class":"form-control","id":"sname","placeholder":"姓名"})
     )
     spassword = forms.CharField(
         error_messages={"required":"密码不能为空"},
-        widget=widgets.PasswordInput(attrs={"class":"form-control","id":"spassword","placeholder":"密码"})
+        widget=dwidgets.PasswordInput(attrs={"class":"form-control","id":"spassword","placeholder":"密码"})
     )
     sex = forms.Field(
         error_messages={"required":"性别不能为空"},
-        widget=widgets.Select(attrs={"class":"form-control","id":"sex"},choices=(("男","男"),("女","女")))
+        widget=dwidgets.Select(attrs={"class":"form-control","id":"sex"},choices=(("男","男"),("女","女")))
     )
     age = forms.IntegerField(
         error_messages={"required":"年龄不能为空"},
-        widget=widgets.TextInput(attrs={"class":"form-control","id":"age","placeholder":"年龄"})
+        widget=dwidgets.TextInput(attrs={"class":"form-control","id":"age","placeholder":"年龄"})
     )
     stelphone = forms.IntegerField(
         error_messages={"required":"电话不能为空"},
-        widget=widgets.TextInput(attrs={"class":"form-control","id":"stelphone","placeholder":"电话号码"})
+        widget=dwidgets.TextInput(attrs={"class":"form-control","id":"stelphone","placeholder":"电话号码"})
     )
     department = forms.Field(
         error_messages={"required":"部门不能为空"},
-        widget=widgets.Select(attrs={"class":"form-control","id":"department"},choices=(("市场","市场"),("财务","财务"),("行政","行政")))
+        widget=dwidgets.Select(attrs={"class":"form-control","id":"department"},choices=(("市场","市场"),("财务","财务"),("行政","行政")))
     )
     role = forms.Field(
         error_messages={"required":"职位不能为空"},
-        widget=widgets.Select(attrs={"class":"form-control","id":"role"},choices=(("销售","销售"),("会计","会计"),("行政","行政")))
+        widget=dwidgets.Select(attrs={"class":"form-control","id":"role"},choices=(("销售","销售"),("会计","会计"),("行政","行政")))
     )
     basic = forms.IntegerField(
         required=False,
         initial=3000,
-        widget=widgets.TextInput(attrs={"class":"form-control","id":"basic","placeholder":"底薪"})
+        widget=dwidgets.TextInput(attrs={"class":"form-control","id":"basic","placeholder":"底薪"})
     )
 
 class Login(forms.Form):
     username = forms.CharField(
         error_messages={"required":"用户名不能为空"},
-        widget=widgets.TextInput(attrs={"class":"placeholder","placeholder":"用户名"})
+        widget=dwidgets.TextInput(attrs={"class":"placeholder","placeholder":"用户名"})
     )
     password = forms.CharField(
         error_messages={"required":"密码不能为空"},
-        widget=widgets.PasswordInput(attrs={"class":"placeholder","placeholder":"密码"})
+        widget=dwidgets.PasswordInput(attrs={"class":"placeholder","placeholder":"密码"})
     )
 
 class Customer(forms.Form):
     cname = forms.CharField(
         error_messages={'required':'用户姓名不能为空'},
-        widget=widgets.TextInput(attrs={'class':'form-control','id':'name','placeholder':'客户姓名'})
+        widget=dwidgets.TextInput(attrs={'class':'form-control','id':'name','placeholder':'客户姓名'})
     )
     ctelphone = forms.CharField(
         error_messages={'required':'客户联系方式不能为空'},
-        widget=widgets.TextInput(attrs={'class':'form-control','id':'ctelphone','placeholder':'电话'})
+        widget=dwidgets.TextInput(attrs={'class':'form-control','id':'ctelphone','placeholder':'电话'})
     )
     car = forms.CharField(
         required=False,
-        widget=widgets.TextInput(attrs={'class':'form-control','id':'car','placeholder':'车型'})
+        widget=dwidgets.TextInput(attrs={'class':'form-control','id':'car','placeholder':'车型'})
     )
 
 class Deal(forms.Form):
-    customer_id = forms.ChoiceField(
-        error_messages={'required':'客户姓名不能为空'},
-        widget=widgets.Select(attrs={"class":"form-control","id":"customer"}),
-        choices=models.Customer.objects.all().values_list('cid', 'cname'),
-    )
-
-    staff_id = forms.ChoiceField(
-        error_messages={'required':'出单员工不能为空'},
-        widget=widgets.Select(attrs={"class":"form-control","id":"staff"}),
-        choices=models.Staff.objects.exclude(role='admin').values_list('sid', 'sname'),
-    )
-
-    product_id = forms.ChoiceField(
-        widget=widgets.Select(attrs={"class":"form-control","id":"product"}),
-        choices=models.Product.objects.all().values_list('pid', 'pname'),
-    )
-    pay = forms.CharField(
+    # customer_id = forms.ModelChoiceField(
+    #     error_messages={'required':'客户姓名不能为空'},
+    #     widget=dwidgets.Select(attrs={"class":"form-control","id":"customer"}),
+    #     # choices=models.Customer.objects.all().values_list('cid', 'cname'),
+    # )
+    #
+    # staff_id = fields.ChoiceField(
+    #     error_messages={'required':'出单员工不能为空'},
+    #     widget=dwidgets.Select(attrs={"class":"form-control","id":"staff"}),
+    #     choices=models.Staff.objects.exclude(role='admin').values_list('sid', 'sname'),
+    # )
+    #
+    # product_id = fields.ChoiceField(
+    #     widget=dwidgets.Select(attrs={"class":"form-control","id":"product"}),
+    #     choices=models.Product.objects.all().values_list('pid', 'pname'),
+    # )
+    pay = fields.CharField(
         error_messages={'required':'成交价不能为空'},
-        widget=widgets.TextInput(attrs={"class":"form-control","id":"price"})
+        widget=dwidgets.TextInput(attrs={"class":"form-control","id":"price"})
     )
-    number = forms.IntegerField(
+    number = fields.IntegerField(
         error_messages={'required':'购买数量不能为空'},
         min_value=1,
         initial=1,
-        widget=widgets.TextInput(attrs={"class":"form-control","id":"number"})
+        widget=dwidgets.TextInput(attrs={"class":"form-control","id":"number"})
     )
 
-    def __init__(self,*args,**kwargs):
-        super(Deal,self).__init__(*args,**kwargs)
-        self.fields['customer_id'].choice = models.Customer.objects.all().values_list('cid', 'cname')
-        self.fields['staff_id'].choice = models.Staff.objects.exclude(role='admin').values_list('sid', 'sname')
-        self.fields['product_id'].choice = models.Product.objects.all().values_list('pid', 'pname')
+    # def __init__(self,*args,**kwargs):
+    #     super(Deal,self).__init__(*args,**kwargs)
+    #     self.fields['customer_id'].choice = models.Customer.objects.all().values_list('cid', 'cname')
+    #     self.fields['staff_id'].choice = models.Staff.objects.exclude(role='admin').values_list('sid', 'sname')
+    #     self.fields['product_id'].choice = models.Product.objects.all().values_list('pid', 'pname')
 
 def auth(func):
     def inner(request,*args,**kwargs):
@@ -199,23 +200,34 @@ def delprodect(request):
 def deal(request):
     role = getrole(request)
     user = request.session['username']
+    all_cus = models.Customer.objects.all().values_list('cid', 'cname')
+    all_staff = models.Staff.objects.filter(role='销售').values_list('sid', 'sname')
+    all_pro = models.Product.objects.all().values_list('pid', 'pname')
     if request.method == "GET":
         dealobj = Deal()
         if role == "admin":
             data = models.Result.objects.all().values('customer__cname','staff__sname','product__pname','product__price','pay','dealtime','number')
         elif role == "sale":
             data = models.Result.objects.filter(staff__sname=user).values('customer__cname','staff__sname','product__pname','product__price','pay','dealtime','number')
-        return render(request,'deal.html',{"role":role,"data":data,"user":user,'obj':dealobj})
+        return render(request,'deal.html',{"role":role,"data":data,"user":user,'obj':dealobj,"all_cus":all_cus,"all_staff":all_staff,"all_pro":all_pro})
     elif request.method == "POST":
         dealobj = Deal(request.POST)
         tag = dealobj.is_valid()
         if tag:
+            deal_data = dealobj.cleaned_data
+            customer_id = request.POST.get('customer_id',None)
+            staff_id = request.POST.get('staff_id',None)
+            product_id = request.POST.get('product_id',None)
+            customer_id,staff_id,product_id = int(customer_id),int(staff_id),int(product_id)
+            deal_data['customer_id'] = request.POST.get('customer_id',None)
+            deal_data['staff_id'] = request.POST.get('staff_id',None)
+            deal_data['product_id'] = request.POST.get('product_id',None)
             res_dic = {}
             stoktag = checkstock(request)
             if stoktag[0]:
                 newstok = stoktag[1]['stok'] - stoktag[1]['number']
                 models.Product.objects.filter(pid=stoktag[1]['pid']).update(stok=newstok)
-                models.Result.objects.create(**dealobj.cleaned_data)
+                models.Result.objects.create(**deal_data)
                 res_dic['status'] = "OK"
             else:
                 res_dic['status'] = 'error'
@@ -228,7 +240,7 @@ def deal(request):
 def checkstock(request):
     pid = request.POST.get('product_id')
     num = request.POST.get('number')
-    num = int(num)
+    pid,num = int(pid),int(num)
     stok = models.Product.objects.filter(pid=pid).values_list('stok')[0][0]
     if  num > stok:
         return (0,{"pid":pid,"stok":stok,"number":num})
